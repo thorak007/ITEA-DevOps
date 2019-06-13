4e4475d063c85ad85e1abb73b6647df164363b6c
thorak007 <45256840+thorak007@users.noreply.github.com>
2019-06-13 15:28:45 +0300

    Update variables.tf

--- IaaC/Terraform/Azure/Lab10/variables.tf
+++ IaaC/Terraform/Azure/Lab10/variables.tf
@@ -5,15 +5,17 @@ variable "prefix" {
 
 variable "location" {
   description = "The Azure Region in which all resources in this example should be provisioned"
-  default="westeurope"
+  default = "westeurope"
 }
 
 variable "kubernetes_client_id" {
   description = "The Client ID for the Service Principal to use for this Managed Kubernetes Cluster"
+  default = "140a7b55-ccde-44b4-9313-4c267dba8f1b"  
 }
 
 variable "kubernetes_client_secret" {
   description = "The Client Secret for the Service Principal to use for this Managed Kubernetes Cluster"
+  default = "1cdf7086-19d5-48dd-81bf-9e26e812bdb1"
 }
 
 
